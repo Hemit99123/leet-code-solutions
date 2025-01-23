ad2bdb3338f905381b04a1c752671db9bbb15946
Hemit Patel <hemitvpatel@gmail.com>
2025-01-23 16:43:15 -0500

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,5 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        # This is our stack which is a DS we will use to retrieve the last element and use that 
+        # to validate the closing parentheses
+        
         stack = []
 
         for item in s:
@@ -14,4 +18,5 @@ class Solution:
                 stack.append(item)
         
         # If the stack is empty, all brackets were matched
+        # The len() function itself returns a boolean so therefore no need for any if/else statements to return a statement
         return len(stack) == 0
