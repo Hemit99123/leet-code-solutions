08feee758158faa31c377ed75ebc74c7e4bb3bc8
Hemit Patel <hemitvpatel@gmail.com>
2025-01-25 22:24:50 -0500

    removed stack class

--- learnings/ds/stacks_learning_excerise/1.py
+++ learnings/ds/stacks_learning_excerise/1.py
@@ -1,41 +1,15 @@
-from collections import deque
-
-class Stack:
-    def __init__(self):
-        self.container = deque()
-    
-    def push(self,val):
-        self.container.append(val)
-        
-    def pop(self):
-        return self.container.pop()
-    
-    def peek(self):
-        return  self.container[-1]
-    
-    def is_empty(self):
-        return len(self.container)==0
-    
-    def size(self):
-        return len(self.container)
-
-
 def reverse_string(text):
-    stack = Stack()
+    stack = []
 
     reverse = ""
 
     for char in text:
         stack.push(char)
-
-    
+        
     for _ in range(stack.size()):
-        new_char = stack.peek()
-
+        new_char = stack.pop()
         reverse += new_char
-
-        stack.pop()
-    
+        
     return reverse
 
 
