7acdf3dd877cd831b78cacfe4c9a7023e83ec64c
Hemit Patel <hemitvpatel@gmail.com>
2025-01-25 19:22:05 -0500

    better solution

--- 150-stack problem.py
+++ 150-stack problem.py
@@ -1,29 +1,26 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        result = 0
+        operators = "+-/*"
 
         for token in tokens:
-            if (token in "+-/*"):
+            if (token in operators):
                 number2 = int(stack.pop())
                 number1 = int(stack.pop())
+                result = 0
 
                 match token:
                     case "+":
-                        result = number1 + number2
+                        result = number1 + number2 
                     case "-":
                         result = number1 - number2
                     case "/":
-                        result = number1 / number2
+                        result = number1 / number2 
                     case "*":
                         result = number1 * number2
 
                 stack.append(result)
             else:
                 stack.append(token)
-        # this is to check if tokens does not have an operand. in that case
-        # the stack would have one number which would be in a string datastructure        
-        if isinstance(stack[-1], str):
-            return int(stack[-1])
-        else:
-            return int(result)
+            
+        return int(stack[0])
